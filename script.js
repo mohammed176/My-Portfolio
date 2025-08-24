b185c22ab1426f6283fbf32fe9cd8446495017e7
mohammed176 <mohamed.1029389@gmail.com>
2025-08-24 14:12:55 +0300

    .

---
+++ script.js
@@ -0,0 +1,27 @@
+// الحصول على الزر وجسم الصفحة
+const theameSwitch = document.getElementById("theameSwitch");
+const body = document.body;
+
+// عند تحميل الصفحة، نحط الوضع السابق من Local Storage
+window.addEventListener("DOMContentLoaded", () => {
+    if(localStorage.getItem("theme") === "light") {
+        body.classList.add("lightmode");
+        theameSwitch.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    } else {
+        body.classList.remove("lightmode");
+        theameSwitch.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    }
+});
+
+// عند الضغط على الزر
+theameSwitch.addEventListener("click", () => {
+    body.classList.toggle("lightmode");
+
+    if(body.classList.contains("lightmode")) {
+        theameSwitch.innerHTML = '<i class="fa-solid fa-sun"></i>';
+        localStorage.setItem("theme", "light");
+    } else {
+        theameSwitch.innerHTML = '<i class="fa-solid fa-moon"></i>';
+        localStorage.setItem("theme", "dark");
+    }
+});
